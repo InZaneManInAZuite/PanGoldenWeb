84af2ac64f8a64f9472d153dca33147a411ed0c1
Toshiro Mendoza <122713720+InZaneManInAZuite@users.noreply.github.com>
2024-08-04 10:17:54 +1200

    Attempt to deploy front end soon

--- frontend/src/Config.ts
+++ frontend/src/Config.ts
@@ -1,10 +1,24 @@
-const config = {
+const configObj = {
     userApiUrl: 'http://localhost:5028/user',
     accountApiUrl: 'http://localhost:5028/account',
     transactionApiUrl: 'http://localhost:5028/transaction',
     apiUrl: 'http://localhost:5028',
-
+    
     pathUrl: 'http://localhost:5173',
   };
+
+  if (process.env.NODE_ENV === 'production') {
+    configObj.userApiUrl = 'https://pangolden-backend-dgh8hkgmbycea8e5.australiasoutheast-01.azurewebsites.net/user';
+    configObj.accountApiUrl = 'https://pangolden-backend-dgh8hkgmbycea8e5.australiasoutheast-01.azurewebsites.net/account';
+    configObj.transactionApiUrl = 'https://pangolden-backend-dgh8hkgmbycea8e5.australiasoutheast-01.azurewebsites.net/transaction';
+    configObj.apiUrl = 'https://pangolden-backend-dgh8hkgmbycea8e5.australiasoutheast-01.azurewebsites.net/';
+    
+    configObj.pathUrl = '';
+  }
+
+const config = configObj;
+  
+
+
   
 export default config;
